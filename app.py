adbda0b0d4800a0ae7be4c30133644fd59ae1d90
Jason Benjoya <jasonbenjoya@gmail.com>
2020-12-09 15:37:45 -0600

    added the cupcake model, and initialized the database using the seed file and configured the app in app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
 """Flask app for Cupcakes"""
+from flask import Flask, request, render_template, jsonify
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'dontyoudaretell'
+
+connect_db(app)
