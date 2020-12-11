f97f70acc111532026cc0097769f4bd05da5652a
Jason Benjoya <jasonbenjoya@gmail.com>
2020-12-11 10:36:02 -0600

    added all CRUD routes and made sure my model was accurate with full serialize function to turn to jsonify-able dict

--- app.py
+++ app.py
@@ -9,3 +9,63 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'dontyoudaretell'
 
 connect_db(app)
+
+
+@app.route('/api/cupcakes')
+def show_all_cupcakes():
+    all_cupcakes = [cupcake.serialize() for cupcake in Cupcake.query.all()]
+
+    return jsonify(cupcakes=all_cupcakes)
+
+
+@app.route('/api/cupcakes/<int:cupcake_id>')
+def show_single_cupcake(cupcake_id):
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+
+    return jsonify(cupcake=cupcake.serialize())
+
+
+@app.route('/api/cupcakes', methods=['POST'])
+def add_cupcake():
+    req = request.json
+
+    new_cupcake = Cupcake(
+        flavor=req['flavor'],
+        size=req['size'],
+        rating=req['rating'],
+        image=request.json.get('image')
+    )
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    return (jsonify(cupcake=new_cupcake.serialize()), 201)
+
+
+@app.route('/api/cupcakes/<int:cupcake_id>', methods=['PATCH'])
+def update_cupcake(cupcake_id):
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+
+    cupcake.flavor = request.json.get('flavor', cupcake.flavor)
+    cupcake.size = request.json.get('size', cupcake.size)
+    cupcake.image = request.json.get('image', cupcake.image)
+    cupcake.rating = request.json.get('rating', cupcake.rating)
+
+    db.session.commit()
+
+    return jsonify(cupcake=cupcake.serialize())
+
+
+@app.route('/')
+def show_home_page():
+    return render_template('index.html')
+
+
+@app.route('/api/cupcakes/<int:cupcake_id>', methods=['DELETE'])
+def delete_cupcake(cupcake_id):
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+
+    db.session.delete(cupcake)
+    db.session.commit()
+
+    return jsonify(message='deleted')
--- models.py
+++ models.py
@@ -21,3 +21,12 @@ class Cupcake(db.Model):
     size = db.Column(db.Text, nullable=False)
     rating = db.Column(db.Float, nullable=False)
     image = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'flavor': self.flavor,
+            'size': self.size,
+            'rating': self.rating,
+            'image': self.image
+        }
